6a11576a2663215a7f7a478c0aed242eed341b35
dsjangid <dsj654@gmail.com>
2026-08-26 16:24:26 +0530

    Refine initial loading screen: minimalist typography, whisper-thin progress line, and compact metric label

---
+++ _next/static/chunks/app/chapters/dossiers/putin/tribunal/page-78f7dbaf7a8b8d03.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[939,391,966,988,968,333,433,641,777,392,537,609,959,598,19,23],{6509:function(e,n,s){Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,3872)),Promise.resolve().then(s.bind(s,659))}},function(e){e.O(0,[448,168,971,117,744],function(){return e(e.s=6509)}),_N_E=e.O()}]);
